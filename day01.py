219a7cb8b1f866893688a61fba4d5c23ad591878
Hans Löfving <hans.lofving@sigma.se>
2023-01-04 15:03:09 +0100

    Rework day 1

--- day01.py
+++ day01.py
@@ -1,29 +1,16 @@
 import sys
 
-print(__file__)
-
-#inputfile = sys.stdin
 inputfile = open("input01")
 allinput = inputfile.readlines()
 
-section = 0
-parsedinput = []
-parsedinput.append([])
-
+parsedinput = [[]]
 for i in allinput:
     if i == '\n':
-        section = section + 1
         parsedinput.append([])
         continue
-    parsedinput[section].append(int(i))
-
-biggest = 0
-for i in parsedinput:
-    summa = sum(i)
-    if summa > biggest:
-        biggest = summa
-
+    parsedinput[-1].append(int(i))
 
 parsedinput.sort(key=lambda a: sum(a))
-
-print(sum(list(map(lambda a: sum(a), parsedinput))[-3:]))
+print("Day 1: Calorie Counting")
+print("Part 1:", sum(parsedinput[-1]))
+print("Part 2:", sum(list(map(sum, parsedinput))[-3:]))
