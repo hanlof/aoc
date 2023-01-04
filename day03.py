9ee4c111dc6447ce150c1dc3cf6cff755723ae12
Hans Löfving <hans.lofving@sigma.se>
2023-01-04 16:40:32 +0100

    Reworked days 3 and 4

--- day03.py
+++ day03.py
@@ -1,41 +1,31 @@
 import sys
 
-print(__file__)
+print("Day 3: Rucksack Reorganization")
 
-#inputfile = sys.stdin
-inputfile = open("input03")
-allinput = inputfile.readlines()
+allinput = open("input03").read().splitlines()
+
+def getprio(c):
+    lettervalue = ord(c)
+    if lettervalue < 97: return lettervalue - ord('A') + 27
+    else: return lettervalue - ord('a') + 1
 
 priosum = 0
 for i in allinput:
-    l = int(len(i) / 2)
-    f = i[0:l]
-    s = i[-(l+1):-1]
-    space = " " * (25 - l) * 2
-    same = set(f) & set(s)
-    asc = ord(list(same)[0])
-    if asc < 97:
-        prio = asc - 65 + 27
-    else:
-        prio = asc - 97 + 1
-
-    print("'%s' %s' %s' %s %d" % (f, space, s, same, prio))
-    priosum = priosum + prio
-
+    halflength = len(i) // 2
+    firsthalf = set(i[0:halflength])
+    secondhalf = set(i[-halflength:])
+    same = firsthalf & secondhalf
+    priosum += getprio(same.pop())
 print("Part 1:", priosum)
 
 priosum = 0
-for i in range(2, len(allinput), 3):
+i = iter(allinput)
+while True:
+    try:
+        a, b, c = next(i), next(i), next(i)
+        same = set(a) & set(b) & set(c)
+        priosum += getprio(same.pop())
+    except:
+        break
+print("Part 2:", priosum)
 
-    same = set(allinput[i-2]) & set(allinput[i-1]) & set(allinput[i])
-    same = filter(lambda c: c != '\n', same)
-    asc = ord(list(same)[0])
-    if asc < 97:
-        prio = asc - 65 + 27
-    else:
-        prio = asc - 97 + 1
-
-    print("'%s' %s' %s' %s %d" % (allinput[i-2], allinput[i-1], allinput[i], same, prio))
-    priosum = priosum + prio
-
-print("Part2:", priosum)
--- day04.py
+++ day04.py
@@ -1,37 +1,17 @@
 import sys
 import re
 
-print(__file__)
+print("Day 4: Camp Cleanup")
+allinput = open("input04").readlines()
 
-#inputfile = sys.stdin
-inputfile = open("input04")
-allinput = inputfile.readlines()
-
-ans = 0
+p1ans, p2ans = 0, 0
 for i in allinput:
     r = re.search("(\d+)-(\d+),(\d+)-(\d+)", i)
-    s1 = int(r[1])
-    e1 = int(r[2])
-    s2 = int(r[3])
-    e2 = int(r[4])
-    if (s1 >= s2 and e1 <= e2): score = 1
-    elif (s2 >= s1 and e2 <= e1): score = 1
-    else: score = 0
-    ans = ans + score
-    print(s1, e1, s2, e2, score)
+    s1, e1, s2, e2 = map(int, r.groups())
+    # part 1: if one range is completely within the other
+    if (s1 >= s2 and e1 <= e2) or (s2 >= s1 and e2 <= e1): p1ans += 1
+    # part 2: if the ranges overlaps at all (using set() is inefficient but fun :)
+    if len(set(range(s1, e1+1)) & set(range(s2, e2+1))) > 0: p2ans += 1
 
-print("Part 1:", ans)
-
-ans = 0
-for i in allinput:
-    r = re.search("(\d+)-(\d+),(\d+)-(\d+)", i)
-    s1 = int(r[1])
-    e1 = int(r[2])
-    s2 = int(r[3])
-    e2 = int(r[4])
-    if len(set(range(s1, e1+1)) & set(range(s2, e2+1))) > 0: score = 1
-    else: score = 0
-    ans = ans + score
-    print(s1, e1, s2, e2, score)
-
-print("Part2:", ans)
+print("Part 1:", p1ans)
+print("Part 2:", p2ans)
