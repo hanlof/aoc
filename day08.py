dee49a3ffbc7844951f32fb086fffdbf07c4d3e3
Hans Löfving <hans.lofving@sigma.se>
2023-01-07 18:44:08 +0100

    day 8 is way fancier! reduced line count from ~120 to ~30

--- day08.py
+++ day08.py
@@ -1,125 +1,74 @@
-import sys
-import re
-import os
+import numpy
 
-print(__file__)
+def p1compact(treematrix):
+    # create new matrix with 2 slots for each tree: original value plus a visibility flag
+    vmap = numpy.zeros( treematrix.shape + (2,) )
+    vmap[..., 0] = treematrix
+    # magically iterate each row AND each column using numpy matrix rotation
+    for treerow in [r for r in vmap] + [c for c in numpy.rot90(vmap)]:
+        for direction in treerow, numpy.flip(treerow, 0): # check both forward and reverse in each tree row
+            visthreshold = 0
+            for tree in direction: # track visibility threshold through the row
+                if tree[0] >= visthreshold: # set trees visibility flag to 1 if it can be seen
+                    tree[1] = 1
+                    visthreshold = tree[0] + 1
+    return int(vmap[...,1].sum()) # return sum of vilibilty flags
 
-#inputfile = sys.stdin
-inputfile = open("inputdata/input08")
-allinput = inputfile.readlines()
+def p2scores(omap):
+    for y in range(omap.shape[0]):
+        for x in range(omap.shape[1]):
+            totscore = 1
+            originheight = omap[y, x]
+            for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                tscore, tx, ty = 0, x + dx, y + dy
+                while 0 <= tx < omap.shape[1] and 0 <= ty < omap.shape[0]:
+                    tscore += 1
+                    if omap[ty, tx] >= originheight: break
+                    tx += dx; ty += dy
+                totscore *= tscore
+            yield totscore
 
-map = [ ]
+allinput = open("inputdata/input08").read().splitlines()
+origmap = numpy.array([[int(char) for char in list(line)] for line in allinput])
+print("Part 1:", p1compact(origmap) )
+print("Part 2:", max(p2scores(origmap)))
 
+# this runs faster than p1compact() so it is kept here for fun.
+# p1fast runs 1000 times in about 4.2s compared to p1compact 6.8s
+treelist = []
 for i in allinput:
-    map.append(list(i[0:-1]))
-
-vismap = []
-vismap = [[0] * len(map[0]) for i in range(len(map))]
-
-for y in range(len(map)):
-    # check from the left
-    visheight = 0
-    for x in range(len(map[y])):
-        height = int(map[y][x])
-        if height >= visheight:
-            vismap[y][x] = 1
-            visheight = height + 1
-    # check from the right
-    visheight = 0
-    for x in reversed(range(len(map[y]))):
-        height = int(map[y][x])
-        if height >= visheight:
-            vismap[y][x] = 1
-            visheight = height + 1
-
-for x in range(len(map[0])):
-    # check from the top
-    visheight = 0
-    for y in range(len(map)):
-        height = int(map[y][x])
-        if height >= visheight:
-            vismap[y][x] = 1
-            visheight = height + 1
-    # check from the right
-    visheight = 0
-    for y in reversed(range(len(map))):
-        height = int(map[y][x])
-        if height >= visheight:
-            vismap[y][x] = 1
-            visheight = height + 1
-
-count = 0
-for i in vismap:
-    for j in i:
-        if j != 0: count = count + 1
-
-print("Part 1:", count)
-
-#for i in vismap:
-#    print(i)
-
-#map = [ [ 3, 0, 3, 7, 3],
-#        [ 2, 5, 5, 1, 2],
-#        [ 6, 5, 3, 3, 2],
-#        [ 3, 3, 5, 4, 9],
-#        [ 3, 5, 3, 9, 0] ]
-
-#vismap = [[0] * len(map[0]) for i in range(len(map))]
-
-def scenic_score(x, y):
-    originheight = map[y][x]
-    # count up
-    visu = 0
-    visd = 0
-    visl = 0
-    visr = 0
-    ty = y - 1
-    while ty >= 0:
-        visu = visu + 1
-        if map[ty][x] >= originheight:
-            break
-        ty = ty - 1
-    # count down
-    ty = y + 1
-    while ty < len(map):
-        visd = visd + 1
-        if map[ty][x] >= originheight:
-            break
-        ty = ty + 1
-    # count left
-    tx = x - 1
-    while tx >= 0:
-        visl = visl + 1
-        if map[y][tx] >= originheight:
-            break
-        tx = tx - 1
-    # count right
-    tx = x + 1
-    while tx < len(map[0]):
-        visr = visr + 1
-        if map[y][tx] >= originheight:
-            break
-        tx = tx + 1
-    return (visu , visd , visl , visr)
-
-vismax = 0
-for y in range(len(map)):
-    for x in range(len(map[y])):
-        vistmp = scenic_score(x, y)
-        vismap[y][x] = vistmp
-        visscore = (vistmp[0] * vistmp[1] * vistmp[2] * vistmp[3])
-        if visscore > vismax:
-            vismax = visscore
-
-#for i in map:
-#    print(i)
-#print()
-#for i in vismap:
-#    print(i)
-
-print("Part 2:", vismax)
-# 5764801 too high
-# 6000099 too high
-
-
+    treelist.append(list(i))
+def p1fast(treelist):
+    visset = set()
+    for y in range(len(treelist)):
+        # check from the left
+        visheight = 0
+        for x in range(len(treelist[y])):
+            height = int(treelist[y][x])
+            if height >= visheight:
+                visset.add( (x, y) )
+                visheight = height + 1
+        # check from the right
+        visheight = 0
+        for x in reversed(range(len(treelist[y]))):
+            height = int(treelist[y][x])
+            if height >= visheight:
+                visset.add( (x, y) )
+                visheight = height + 1
+    for x in range(len(treelist[0])):
+        # check from the top
+        visheight = 0
+        for y in range(len(treelist)):
+            height = int(treelist[y][x])
+            if height >= visheight:
+                visset.add( (x, y) )
+                visheight = height + 1
+        # check from the right
+        visheight = 0
+        for y in reversed(range(len(treelist))):
+            height = int(treelist[y][x])
+            if height >= visheight:
+                visset.add( (x, y) )
+                visheight = height + 1
+    return len(visset)
 
