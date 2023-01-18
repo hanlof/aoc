d14cc756892ff673a99ed6bc14b0f30a6688b47c
Hans Löfving <hans.lofving@sigma.se>
2023-01-18 23:20:36 +0100

    Day 20 greatly improved and also added aoc.py (AoC utility library)
    cleaned up and removed useless code that apparently took 90% of total runtime (lol)

---
+++ aoc.py
@@ -0,0 +1,98 @@
+import os
+import re
+import inspect
+import itertools
+import time
+
+verbose = 1
+
+class Timing():
+    def __init__(s, label=None, precision=2):
+        s.timestamps = list()
+        s.add(label)
+        s.precision = precision
+    def add(s, label=None):
+        s.timestamps.append( (label, time.time() ) )
+    def print(s):
+        if s.timestamps[0][0] is not None:
+            print(s.timestamps[0][0])
+        for t1, t2 in itertools.pairwise(s.timestamps):
+            print(t2[0], ":", round(t2[1] - t1[1], s.precision),
+                    round(t2[1] - s.timestamps[0][1], s.precision))
+
+def firstcallerstackframe():
+    callstack = inspect.stack()
+    for frame in callstack:
+        if frame.filename == callstack[0].filename:
+            continue
+        caller = frame
+        break
+    return caller
+
+def getverbosity():
+    callerframe = firstcallerstackframe()
+    global verbose
+    if "verbose" in callerframe.frame.f_globals:
+        verbose = callerframe.frame.f_globals['verbose']
+    if "verbose" in callerframe.frame.f_locals:
+        verbose = callerframe.frame.f_locals['verbose']
+    return verbose
+
+def metadata(day=None):
+    if day is None:
+        callerframe = firstcallerstackframe()
+        pyfile = callerframe.filename
+        r = re.search("day(\d+)", pyfile)
+        if r[1]:
+            day = int(r[1])
+        else:
+            day = None
+    pyfile = "day%02d.py" % day
+    basepath = os.path.dirname(pyfile)
+    inputfname = os.path.join(basepath, "inputdata/input%02d" % day)
+    htmlfname = os.path.join(basepath, "htmlpages/%d.html" % day)
+    return {'pyfile': pyfile, 'daynb': day, 'basepath': basepath, 'inputfname': inputfname, 'htmlfname': htmlfname }
+
+def getinput(day=None, conv=None):
+    verbose = getverbosity()
+    cinfo = metadata()
+    if day is None:
+        inputfilename = cinfo['inputfname']
+    else:
+        inputfilename = os.path.join(cinfo['basepath'], "inputdata/input%02d" % day)
+    if verbose > 2: print("Input file is:", inpfilepath)
+    if not os.path.exists(inputfilename):
+        # XXX try to fetch it using ./fetch_input ?!
+        raise Exception("Input file'" + inputfilename + "'not found")
+    lines = open(inputfilename).read().splitlines()
+    if conv is not None: return map(conv, lines)
+    return lines
+
+
+# splits input into sections by empty lines
+# and converts the types of elements
+def sections(elements, conv=None):
+    if conv is None: t = lambda x: x
+    else: t = conv
+    sections = itertools.groupby(elements, key=lambda l: l == "")
+    return [[t(l) for l in sec] for empty, sec in sections if not empty]
+
+def htmlcodesections(day=None):
+    c = callerinfo()
+    s = open(c['htmlfname']).read()
+    a = re.findall("<pre><code>(.*)?</code></pre>", s, flags=re.MULTILINE | re.DOTALL)
+    return [s.splitlines() for s in a]
+
+def htmlanswers(day=None):
+    c = metadata(day)
+    s = open(c['htmlfname']).read()
+    a = re.findall("Your puzzle answer was <code>(.*?)</code>", s, flags=re.MULTILINE | re.DOTALL)
+    return a
+
+def htmltitle(day=None):
+    c = metadata(day)
+    s = open(c['htmlfname']).read()
+    a = re.findall("day-desc..<h2>(.*?)</h2>", s, flags=re.MULTILINE | re.DOTALL)
+    return a[0]
+    """day-desc..<h2>.*</h2>"""
+
--- day20.py
+++ day20.py
@@ -1,24 +1,16 @@
-inputlines = open("inputdata/input20").readlines()
-l = list(map(int, inputlines))
-#l = [1, 2, -3, 3, -2, 0, 4]
+import aoc
 
 def mix(l, times):
     t = list(range(len(l)))
-    if len(l) < 20:
-        print("Init:", l)
-    for i in range(times):
-        for i in range(len(l)):
-            steps = l[i]
-            assert i in t, t[:10]
+    while times > 0:
+        for i, steps in enumerate(l):
             pos = t.index(i)
             newpos = (pos + steps) % (len(l) - 1)
 
             t.pop(pos)
             t.insert(newpos, i)
 
-            l2 = list([l[i] for i in t])
-            if len(l) < 20 and times == 1:
-                print("     ", l2)
+        times -= 1
     return t
 
 def groovecoords(t, l):
@@ -27,10 +19,11 @@ def groovecoords(t, l):
     c1 = reordered[(zeropos + 1000) % len(t)]
     c2 = reordered[(zeropos + 2000) % len(t)]
     c3 = reordered[(zeropos + 3000) % len(t)]
-    print("grove", c1, c2, c3)
+    #print("grove", c1, c2, c3)
     return c1 + c2 + c3
 
 
+l = list(aoc.getinput(conv=int))
 t = mix(l, 1)
 print("Part 1:", groovecoords(t, l))
 
