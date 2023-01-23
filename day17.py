c659928be0fd9cf4bf30465dd12f4c9a26e2651c
Hans Löfving <hans.lofving@sigma.se>
2023-01-23 23:55:02 +0100

    Day 17 time reduced from ~1.5 to ~0.2s... some cleanup still to be done

--- day17.py
+++ day17.py
@@ -1,3 +1,7 @@
+import aoc
+import itertools
+import sys
+
 class RockGenerator():
     ROCKS = list([
         [ "       ", "       ", "       ", "  #    ", "       "],
@@ -6,14 +10,27 @@ class RockGenerator():
         [ "  #### ", "   #   ", "  ###  ", "  #    ", "  ##   "]])
     def __init__(s):
         s.rocknum = 0
+        s.binrocks = dict()
+        for r in range(5):
+            binrock = 0
+            for i in range(4):
+                binrock <<= 8
+                binrock += int("".join(["1" if c == "#" else "0" for c in s.ROCKS[i][r] + "0"]), 2)
+                #binrock <<= 1 # leave empty space in rightmost bit
+            s.binrocks[r] = binrock
+        s.biniter = itertools.cycle(s.binrocks.values())
     def next(s):
         r = s.rocknum
         s.rocknum = (r + 1) % 5
         return [s.ROCKS[0][r], s.ROCKS[1][r], s.ROCKS[2][r], s.ROCKS[3][r]]
+    def nextbin(s):
+        return next(s.biniter)
 
 class Piece():
-    def __init__(s, lines, yline):
+    def __init__(s, lines, yline, binrock):
         s.coords = set()
+        s.bin = binrock
+        s.binylevel = yline
         for y in range(len(lines)):
             for x in range(len(lines[y])):
                 if lines[y][x] == "#": s.coords.add( (x, yline - y) )
@@ -24,30 +41,38 @@ class Piece():
     def moveddown(s):
         return set([(x, y - 1) for x, y in s.coords])
     def trymoveleft(s, f):
-        newcoord = s.movedleft()
-        if min(next(zip(*newcoord))) < 0:
-            return False
-        for c in newcoord:
-            if c in f.rocks: return False
-        s.coords = newcoord
+        if (s.bin << 1) & 0x101010100: return False
+        if f.binrocks[s.binylevel - 0] & ((s.bin >> 23) & 0xff): return False
+        if f.binrocks[s.binylevel - 1] & ((s.bin >> 15) & 0xff): return False
+        if f.binrocks[s.binylevel - 2] & ((s.bin >> 7) & 0xff): return False
+        if f.binrocks[s.binylevel - 3] & ((s.bin << 1) & 0xff): return False
+        s.bin <<= 1
         return True
     def trymoveright(s, f):
-        newcoord = s.movedright()
-        if max(next(zip(*newcoord))) > 6:
-            return False
-        for c in newcoord:
-            if c in f.rocks: return False
-        s.coords = newcoord
+        if (s.bin >> 1) & 0x01010101: return False
+        if f.binrocks[s.binylevel - 0] & ((s.bin >> 25) & 0xff): return False
+        if f.binrocks[s.binylevel - 1] & ((s.bin >> 17) & 0xff): return False
+        if f.binrocks[s.binylevel - 2] & ((s.bin >> 9) & 0xff): return False
+        if f.binrocks[s.binylevel - 3] & ((s.bin >> 1) & 0xff): return False
+        s.bin >>= 1
         return True
     def trymovedown(s, f):
-        newcoord = s.moveddown()
-        for c in newcoord:
-            if c in f.rocks:
-                f.stick(s.coords)
-                return False
-        s.coords = newcoord
+        if f.binrocks[s.binylevel - 1] & ((s.bin >> 24) & 0xff):
+            f.stickbin(s)
+            return False
+        if f.binrocks[s.binylevel - 2] & ((s.bin >> 16) & 0xff):
+            f.stickbin(s)
+            return False
+        if f.binrocks[s.binylevel - 3] & ((s.bin >> 8) & 0xff):
+            f.stickbin(s)
+            return False
+        if f.binrocks[s.binylevel - 4] & ((s.bin >> 0) & 0xff):
+            f.stickbin(s)
+            return False
+        s.binylevel -= 1
         return True
     def move(s, f, movement):
+        f.updatebinrocks(s.binylevel)
         if "<" == movement:
             p.trymoveleft(f)
         elif ">" == movement:
@@ -56,12 +81,25 @@ class Piece():
             assert False, "Unknown movement %s" % movement.__repr__()
 
 class Field():
+    count = 0
     def __init__(s):
         s.rocks = set( map(lambda x: (x, 0), range(7)) )
+        s.binrocks = { 0: 0xff }
         s.rgen = RockGenerator()
         s.highest = 0
     def __str__(s):
         return s.lines
+    def updatebinrocks(s, ylevel):
+        while not ylevel in s.binrocks:
+            s.binrocks[ylevel] = 0
+            ylevel -= 1
+    def stickbin(s, piece):
+        s.binrocks[piece.binylevel - 0] |= (piece.bin >> 24) & 0xff
+        s.binrocks[piece.binylevel - 1] |= (piece.bin >> 16) & 0xff
+        s.binrocks[piece.binylevel - 2] |= (piece.bin >> 8) & 0xff
+        s.binrocks[piece.binylevel - 3] |= (piece.bin >> 0) & 0xff
+        while s.binrocks[s.highest + 1] != 0:
+            s.highest += 1
     def stick(s, coords):
         s.rocks.update(coords)
         top = 0
@@ -69,89 +107,108 @@ class Field():
             if y > top: top = y
         if top > s.highest: s.highest = top
 
-def prune(queue):
-    possiblejumps = \
-        [( 1, 0), (-1, 0), (0, 1)]
-    s = 0
+# find all lines that can be interacted with from above (by new pieces)
+def calcactivelines(f):
+    possiblejumps = [( 1, 0), (-1, 0), (0, 1)]
     visited = set()
     finalset = set()
+    queue = [ (0, 0) ]
+    lowest_y = 0
+    count = 0
     while len(queue) > 0:
         coord = queue.pop()
         if coord in visited:
             continue
         visited.add(coord)
-        x = coord[0]
-        y = coord[1]
-        for trycoord in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]:
-            if trycoord in f.rocks:
-                finalset.add(trycoord)
-            elif trycoord[0] >= 0 and trycoord[0] <= 6 \
-                    and trycoord[1] <= (f.highest + 1) and not trycoord in visited:
-                queue.append(trycoord)
-    f.rocks = finalset
+        x, y = coord
+        for dx, dy in possiblejumps:
+            x, y = x + dx, y + dy
+            if x < 0: continue
+            if x > 6: continue
+            if (f.highest - y) < 0: continue
+            lowest_y = max(lowest_y, y)
+            if (0x80 >> x) & f.binrocks[f.highest - y]:
+                pass
+            elif not (x, y) in visited:
+                count += 1
+                queue.append((x, y))
+    return lowest_y
 
+tim = aoc.Timing("Timing")
 allinput = open("inputdata/input17").readline()
 
-import itertools
 
 r = RockGenerator()
 f = Field()
 movements = iter(itertools.cycle(allinput[:-1]))
-for i in range(2022):
-    p = Piece(r.next(), f.highest + 7)
-    p.move(f, next(movements, None))
-    while p.trymovedown(f):
-        p.move(f, next(movements, None))
-    prune( [ (0, f.highest + 1) ] )
+count = 0
+savedstates = dict()
+keysforstate = list()
+heightforstate = list()
+prevmatched = False
 
-print("Part 1:", f.highest)
-
-r = RockGenerator()
-f = Field()
-a = dict()
-longestdelta = 0
-longestiters = 0
-movements = iter(itertools.cycle(allinput[:-1]))
-
-INITIALPIECES = 10000
-for i in range(INITIALPIECES):
-    #    if (i % (len(allinput) - 1)) == 0:
-    #        print(len(f.rocks), end=" ")
-    #        prune( [ (0, f.highest + 1) ] )
-    #        #print(i, f.highest / (i+1), len(f.rocks))
-    #        newcombo = (len(f.rocks), r.rocknum)
-    #        if newcombo in a:
-    #            print("COOL--------------------------", end="")
-    #        print(len(f.rocks), r.rocknum)
-    #        a[newcombo] = "haoeu"
-    prune( [ (0, f.highest + 1) ] )
-    newset = tuple(sorted([(x, y - f.highest) for x, y in f.rocks], key=lambda x: x[0] + x[1] * 19001))
-    newset = newset + (r.rocknum,)
-    if newset not in a:
-        if len(newset) < 10:
-            print(i, len(a), newset)
-        a[newset] = (f.highest, i)
+#for i in range(2022):
+matchesinarow = 0
+prevlen = 0
+samelencount = 0
+highestdelta = 0
+P1CYCLE = 2022
+p1ans = 0
+for i in itertools.count():
+    activelines_count = calcactivelines(f)
+    hashkey = tuple([r.rocknum] + [f.binrocks[f.highest - j] for j in range(activelines_count)])
+    if i == P1CYCLE: p1ans = f.highest
+    keysforstate.append(hashkey)
+    heightforstate.append(f.highest)
+    prevkey = savedstates.get(hashkey)
+    savedstates[hashkey] = i
+    if prevlen == len(savedstates):
+        samelencount += 1
     else:
-        height, count = a[newset]
-        if i > 3000:
-            if f.highest - height > longestdelta: longestdelta = f.highest - height
-            if i - count > longestiters: longestiters = i - count
-        a[newset] = (f.highest, i)
-    p = Piece(r.next(), f.highest + 7)
+        samelencount = 0
+    if samelencount >= len(savedstates):
+        delta = i - prevkey
+        highestdelta = max(delta, highestdelta)
+        temp = 0
+        for j in range(i - highestdelta, i):
+            if keysforstate[j] != keysforstate[j - highestdelta]:
+                break
+            temp += 1
+        if temp == highestdelta:
+            print("first repeated cycle is at", i, "with len", highestdelta, "height diff", heightforstate[i] - heightforstate[i - highestdelta])
+            break
+
+    if samelencount >= len(savedstates):
+        pass
+
+    prevlen = len(savedstates)
+
+    p = Piece(r.next(), f.highest + 7, r.nextbin())
     p.move(f, next(movements, None))
     while p.trymovedown(f):
         p.move(f, next(movements, None))
 
+print("Part 1:", p1ans)
+
+tim.add("P1")
+
+longestiters = 1740
+i = 3922
+longestdelta = 2666
+tim.add("Find repetition")
 ONE_TRILLION = 1000 * 1000 * 1000 * 1000
-print(longestiters, longestdelta)
-times, remaining = divmod(ONE_TRILLION - INITIALPIECES, longestiters)
+times, remaining = divmod(ONE_TRILLION - (i), longestiters)
 
 for i in range(remaining):
-    p = Piece(r.next(), f.highest + 7)
+    p = Piece(r.next(), f.highest + 7, r.nextbin())
     p.move(f, next(movements, None))
     while p.trymovedown(f):
         p.move(f, next(movements, None))
 
 print("Part 2:", f.highest + times * longestdelta)
+tim.add("remain")
+tim.print()
+
+print(aoc.htmlanswers())
 
 
