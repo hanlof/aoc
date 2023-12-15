b992e79ba74fc2a118e5d60e4d788f0bd8094120
Hans Löfving <hans.lofving@sigma.se>
2023-12-15 19:47:08 +0100

    day10 update

--- 2023/day10.py
+++ 2023/day10.py
@@ -3,23 +3,10 @@ import aoc
 # 140 x 140
 pipesbycoord = dict()
 startcoord = 0
-adjacent = {(x + y * 1j) for x in [-1, 0, 1] for y in [-1, 0, 1]} - { (0j) }
-pipeexits = {
-    '-': [ -1, 1],
-    '|': [-1j, 1j],
-    '7': [ -1, 1j],
-    'F': [  1, 1j],
-    'J': [ -1, -1j],
-    'L': [  1, -1j] }
-
-pipeturns = { # in the direction of pipeexits, clockwise = 1, ccw = -1
-    '-': 0,
-    '|': 0,
-    '7': 1,
-    'F': -1,
-    'J': -1,
-    'L': 1 }
-
+pipeexits = { '-': [ -1, 1],  '|': [-1j, 1j],  '7': [ -1, 1j],
+              'F': [  1, 1j], 'J': [ -1, -1j], 'L': [  1, -1j] }
+# in the direction of pipeexits, clockwise = 1, ccw = -1
+pipeturns = { '-': 0, '|': 0, '7': 1, 'F': -1, 'J': -1, 'L': 1 }
 dir2posdelta = { 0: -1j, 1: 1, 2: 1j, 3: -1 }
 posdelta2dir = dict([[v, k] for k, v in dir2posdelta.items()])
 
@@ -30,44 +17,37 @@ for y, l in enumerate(aoc_inputlines):
             startcoord = x + y * 1j
 
 # insert the correct pipe into starting position
-matchingdirections = list()
-for a in adjacent:
-    e = pipesbycoord[startcoord + a]
-    if startcoord + a + pipeexits[e][0] == startcoord:
-        matchingdirections.append(a)
-    if startcoord + a + pipeexits[e][1] == startcoord:
-        matchingdirections.append(a)
+startingexits = set()
+for a in [1, -1, 1j, -1j]:
+    for e in pipeexits[pipesbycoord[startcoord + a]]:
+        if startcoord + a + e == startcoord:
+            startingexits |= {a}
+for shape, exits in pipeexits.items():
+    if set(exits) == startingexits:
+        pipesbycoord[startcoord] = shape
 
-for k, e in pipeexits.items():
-    if set(e) == set(matchingdirections):
-        pipesbycoord[startcoord] = k
-
-distance = { startcoord: 0 }
+distances = { startcoord: 0 }
 pipes = [ startcoord ]
-maxdist = 0
 # iterate both directions at the same time
 while pipes:
     coord = pipes.pop()
     shape = pipesbycoord[coord]
-    for direction in pipeexits[shape]:
-        newcoord = coord + direction
-        if newcoord not in distance:
-            newdist = distance[coord] + 1
-            maxdist = max(maxdist, newdist)
-            distance[newcoord] = newdist
+    for posdelta in pipeexits[shape]:
+        newcoord = coord + posdelta
+        if newcoord not in distances:
+            distances[newcoord] = distances[coord] + 1
             pipes.insert(0, newcoord)
 
-print("Part 1:", maxdist)
-
-DIRECTION = 0
-prevcoord = startcoord + pipeexits[pipesbycoord[startcoord]][DIRECTION]
-startdir = posdelta2dir[pipeexits[pipesbycoord[startcoord]][DIRECTION]]
-startdir = (startdir + 2) % 4 # startdir is oposite the direction of exit 1
-curcoord = startcoord
-curdir = startdir
+print("Part 1:", max(distances.values()))
 
 # this loop finds the looping direction of the path around the circular pipe in
 # the direction going towards startcoords first exit. CW or CCW
+TRAVERSEDIR = 0 # select exit 0 or 1 to start traversing from the starting coordinate
+prevcoord = startcoord + pipeexits[pipesbycoord[startcoord]][TRAVERSEDIR]
+startdir = posdelta2dir[pipeexits[pipesbycoord[startcoord]][TRAVERSEDIR]]
+startdir = (startdir + 2) % 4 # startdir is oposite the direction from start to prev
+curcoord = startcoord
+curdir = startdir
 while True:
     shape = pipesbycoord[curcoord]
     for posdelta in pipeexits[shape]:
@@ -82,23 +62,16 @@ while True:
     curcoord = nextcoord
     curdir += dirdelta
     if curcoord == startcoord: break
-
-insidedir = (curdir - startdir) // abs(curdir - startdir)
+insideturndirection = (curdir - startdir) // abs(curdir - startdir)
 
 # the direction of the inside relative to the traversing direction is determined
-# now traverse the pipe again and mark all squares on the inside side as inside
-prevcoord = startcoord + pipeexits[pipesbycoord[startcoord]][DIRECTION]
-startdir = posdelta2dir[pipeexits[pipesbycoord[startcoord]][DIRECTION]]
+# now traverse the pipe again and mark all squares on this side as inside squares
+prevcoord = startcoord + pipeexits[pipesbycoord[startcoord]][TRAVERSEDIR]
+startdir = posdelta2dir[pipeexits[pipesbycoord[startcoord]][TRAVERSEDIR]]
 startdir = (startdir + 2) % 4 # startdir is oposite the direction of exit 1
 curcoord = startcoord
 curdir = startdir
 inside = set()
-
-
-# XXX Currently this loop also finds all inside squares assuming
-# that "inside" is to the right of every square in the pipe loop
-# XXX needs to be tidied up ok thanks!
-
 while True:
     shape = pipesbycoord[curcoord]
     for posdelta in pipeexits[shape]:
@@ -109,31 +82,26 @@ while True:
         dirdelta = -pipeturns[shape]
     else:
         dirdelta = pipeturns[shape]
-    totheright = curcoord + dir2posdelta[(curdir + insidedir) % 4]
-    if totheright not in distance:
-        inside |= { totheright }
-    totheright = prevcoord + dir2posdelta[(curdir + insidedir) % 4]
-    if totheright not in distance:
-        inside |= { totheright }
-
+    for c in curcoord, prevcoord:
+        insidecoord = c + dir2posdelta[(curdir + insideturndirection) % 4]
+        if insidecoord not in distances:
+            inside |= { insidecoord }
     prevcoord = curcoord
     curcoord = nextcoord
     curdir += dirdelta
     if curcoord == startcoord: break
 
-def floodfill(collection):
-    filled = 0
+def floodfill(collection, limit):
     newset = set()
     for c in collection:
         for posdelta in [ -1j, 1j, -1, 1 ]:
             trycoord = c + posdelta
-            if trycoord not in collection and trycoord not in distance:
-                filled += 1
+            if trycoord not in collection and trycoord not in limit:
                 newset |= { c + posdelta }
     collection |= newset
     return len(newset)
 
-while floodfill(inside) > 0: pass
+while floodfill(inside, distances) > 0: pass
 print("Part 2:", len(inside))
 
 def coloredprint():
@@ -145,8 +113,8 @@ def coloredprint():
                 col = "\x1b[1;31m"
             elif coord in inside:
                 col = "\x1b[0;35m"
-            elif coord in distance:
+            elif coord in distances:
                 col = "\x1b[33m"
             print(col, pipesbycoord[coord], "\x1b[0m", sep="", end="")
         print()
-#coloredprint()
+coloredprint()
